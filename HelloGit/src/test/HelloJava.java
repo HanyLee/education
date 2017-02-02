bcee38c6104ec1374b47671c9cc4ecb83b6ff2a7
acorn <acorn@acorn-PC>
2017-02-02 11:07:12 +0900

    Second Commit Work

--- HelloGit/src/test/HelloJava.java
+++ HelloGit/src/test/HelloJava.java
@@ -7,6 +7,9 @@ public class HelloJava {
 		System.out.println("HELLO JAVA~~~");
 		System.out.println("First Commit Work!!!!");
 		System.out.println("아아 지금은 Git 실습 중입니다.");
+		
+		// branch 추가 내용
+		System.out.println("Second Commit Work");
 
 	}
 
